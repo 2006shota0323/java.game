12345b4b299d599f2684af01a62970fb5e71a5ef
Shota0323 <shotawatanabe03230@gamil.com>
2025-06-17 11:13:10 +0900

    クラスDropEnemyの追加 11.15

---
+++ java.game/src/my/game/DropEnemy.java
@@ -0,0 +1,15 @@
+package my.game;
+
+public class DropEnemy extends Enemy
+{
+	public DropEnemy(double x,double y, double vx,double vy)
+	{
+		super(x,y,vx,vy);
+	}
+	
+	public void move()
+	{
+		super.move();
+		vy=vy+0.1;
+	}
+}
